46f7ac28a49ec675df31507465c2cc01b88f1129
Keno <keno@Kenos-MacBook-Air.local>
2025-10-29 11:41:09 +0800

    ”10/29k”

--- main/Assets/Code/slimeAI.cs
+++ main/Assets/Code/slimeAI.cs
@@ -4,37 +4,41 @@ using UnityEngine;
 public class slimeAI : MonoBehaviour{
     public enum Status{idle, walk};
     public Status status;
-    public enum Face{right, left};
+    public enum Face{Right, Left};
     public Face face;
     public float speed;
+    private Transform myTransform;
+    public Transform playerTransform;
 
     void Start()
     {
-        status = Status.idle;
+        status = Status.walk;
         if (this.transform.GetComponent<SpriteRenderer>().flipX)
         {
-            face = Face.left;
+            face = Face.Left;
         }
         else
         {
-            face = Face.right;
+            face = Face.Right;
         }
-
+        myTransform = this.transform;
     }
     void Update()
     {
+        float deltaTime = Time.deltaTime;
         switch (status)
         {
             case Status.idle:
                 break;
             case Status.walk:
+            
                 switch (face)
                 {
-                    case Face.right:
-
+                    case Face.Right:
+                        myTransform.position += new Vector3(speed*deltaTime,0,0);
                         break;
-                    case Face.left:
-
+                    case Face.Left:
+                        myTransform.position -= new Vector3(speed*deltaTime,0,0);
                         break;
                 }
                 break;
