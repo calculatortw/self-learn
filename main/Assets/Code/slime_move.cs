e208c6572f4c1b3ce4d2dcf9e44c11024aa5bb28
Keno <keno@Kenos-MacBook-Air.local>
2026-04-01 11:48:14 +0800

    20260401

--- main/Assets/Code/slime_move.cs
+++ main/Assets/Code/slime_move.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 public class slime_idle : MonoBehaviour
 {
     public Transform[] PatrolPoints;
     public float speed;
-    public int PPDS;
+    public int PPDS; // 巡邏終點
+    public Transform playerTransform;
+    public bool Chasing;
+    public float cDS; // 追蹤距離
     void Update()
     {
-        if (PPDS == 0)
+        if (Chasing)
+        {
+            if (transform.position.x > playerTransform.position.x)
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if (transform.position.x < playerTransform.position.x)
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+
+        }
+        else
+        {
+            if (Vector2.Distance(transform.position, playerTransform.position) < cDS)
+            {
+                Chasing = true;
+            }
+            if (PPDS == 0)
         {
             transform.position = Vector2.MoveTowards(transform.position, PatrolPoints[0].position, speed * Time.deltaTime);
             if (Vector2.Distance(transform.position, PatrolPoints[0].position) < 0.1f)
@@ -26,5 +48,8 @@ public class slime_idle : MonoBehaviour
             }
 
         }
+
+        }
+
     }
 }
