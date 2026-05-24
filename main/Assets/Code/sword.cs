4dbdb736c8ad8a85074bbb0ed4b67d0ac6c0e290
calculatortw <a311558@stu.tnssh.tn.edu.tw>
2026-05-24 21:12:01 +0800

    0524

--- main/Assets/Code/sword.cs
+++ main/Assets/Code/sword.cs
@@ -6,6 +6,8 @@ public class sword : MonoBehaviour
 {
     private GameObject object0;
     public Transform playertransform;
+    private float atk = 0.5f;
+    private float nextatk = 0.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,9 +18,10 @@ public class sword : MonoBehaviour
     void Update()
     {
         object0.transform.position = new Vector3(playertransform.position.x, playertransform.position.y, object0.transform.position.z);
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time>nextatk)
         {
-            transform.Rotate(0, 0, 3f);  
+            nextatk = Time.time + atk;
+            transform.Rotate(0, 0, -90f);  
         }
     }
 }
